dbec3b9bd457b3da01df1107e4ed2e4e7f88b528
Tanguy Robilliard <48035168+Tanguyrob@users.noreply.github.com>
2019-07-24 17:00:00 +0200

    encore bonsoir

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def hello():
 def signup():
     return render_template('signup.html')
 
-
+'''
 @app.route("/donnees", methods='POST')
 def donnees():
     if request.method:
@@ -33,9 +33,27 @@ def donnees():
         soiree = request.form['soiree']
         comments = request.form['comments']
         pays = request.form['pays']
+'''   
     
-    
-    
+@app.route("/signin", methods=["GET", "POST"])
+def signin():
+    error = None
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+        password_hash = hashlib.sha256(str(password).encode("utf-8")).hexdigest()
+
+        req_connection_client = "SELECT * FROM Client where Email = '%s' AND PasswordHash = '%s' "
+        cursor.execute(req_connection_client % (email, password_hash))
+        resultat_connection_client = cursor.fetchall()
+
+        if len(resultat_connection_client) == 0:
+            session['email'] = None
+            error = "Cette adresse courriel ou ce mot de passe ne sont pas valides, veuillez reessayer"
+            return render_template("signin.html", error=error)
+
+    elif request.method == "GET":
+        return render_template("signin.html")
 
 if __name__== "__main__":
-    app.run(port=8000)
+    app.run()
